06d78456fcf7a27c9c7411f64985cb46351b3960
baskar142 <baskarpmkevin@gmail.com>
2025-06-13 20:17:14 +0530

    data ingestion added

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Visa-Classification-MLOPS
+
+
+How to run?
+
+conda create -n visa python=3.8 -y
+
+conda activate visa
+
+pip install -r requirements.txt
+
+Workflow:
+
+1.Constants
+2.entity
+3.componets
+4.pipeline
+5.Main file
+
--- demo.py
+++ demo.py
@@ -1,10 +1,4 @@
-from us_visa.logger import logging
-from us_visa.exception import USvisaException
-import sys
+from us_visa.pipline.training_pipeline import TrainPipeline
 
-logging.info("Logging is done.")
-
-try:
-    a= 1 / 0
-except Exception as e:
-    raise USvisaException(e, sys) from e
+obj = TrainPipeline()
+obj.run_pipeline()
